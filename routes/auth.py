40e2721a71578997dde72fa3e95abde9ce5f9b44
blackprince001 <appiahboaduprince@gmail.com>
2023-07-14 21:07:38 +0000

    setup client for easier authentication

--- routes/auth.py
+++ routes/auth.py
@@ -1,6 +1,14 @@
 from fastapi import APIRouter
+from supabase import Client, create_client
+
+from compax_api.config import get_settings
+
+config = get_settings()
 
 auth = APIRouter()
+
+url, key = config.supabase_uri, config.supabase_key
+supabase: Client = create_client(supabase_url=url, supabase_key=key)
 
 
 @auth.get("", tags=["auth"])
