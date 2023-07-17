170fe908fc33cc38fc4231bc14ce69787a3d7042
blackprince001 <appiahboaduprince@gmail.com>
2023-07-17 06:16:07 +0000

    replace connections with connections pools

--- compax_api/database.py
+++ compax_api/database.py
@@ -1,8 +1,20 @@
-from sqlalchemy import create_engine
+import asyncpg
 
 from .config import get_settings
 
+config = get_settings()
+
+database_credentials = config.dict()
+
+
+def get_db_conn():
+    with asyncpg.create_pool(
+        **database_credentials,
+        min_size=0,
+        max_size=200,
+    ) as pool:
+        yield pool
+
+
 # connecting to the supabase database
-engine = create_engine(
-    get_settings().supabase_uri, connect_args={"check_same_thread": False}
-)
+# engine = create_engine(connect_args={"check_same_thread": False})
