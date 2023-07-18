c1b73111d5eba17782a7f41a74ece75949aa4c7f
blackprince001 <appiahboaduprince@gmail.com>
2023-07-18 17:37:52 +0000

    merge user and admin schema

--- schema/user.py
+++ schema/user.py
@@ -11,3 +11,27 @@ class UserCreate(UserBase):
 
 class User(UserBase):
     pass
+
+
+class AdminBase(UserBase):
+    pass
+
+
+class AdminCreate(AdminBase):
+    pass
+
+
+class Admin(AdminBase):
+    pass
+
+
+class ExamOfficer(UserBase):
+    pass
+
+
+class ExamOfficerCreate(ExamOfficer):
+    pass
+
+
+class ExamOfficer(ExamOfficer):
+    pass
