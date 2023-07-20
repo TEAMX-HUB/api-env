d149773d3870076cb1c4e1ce180c10bcd9f0cdd6
blackprince001 <appiahboaduprince@gmail.com>
2023-07-20 19:52:22 +0000

    initial error handling classes

--- compax_api/errors.py
+++ compax_api/errors.py
@@ -1,12 +1,54 @@
+from uuid import uuid4 as UUID
+
 from fastapi import HTTPException
 
 
-class UserNotFound(HTTPException):
-    pass
+class CompaxException(HTTPException):
+    def __init__(self, status_code: int, detail: str):
+        super().__init__(status_code, detail=detail)
 
 
-class AdminNotFound(HTTPException):
-    pass
+class BuildingNotFoundException(CompaxException):
+    def __init__(self, building_id: str):
+        detail = f"Building with ID {building_id} not found."
+        super().__init__(status_code=404, detail=detail)
 
 
-...  # more exceptions should be added here
+class ClassroomNotFoundException(CompaxException):
+    def __init__(self, classroom_id: str):
+        detail = f"Classroom with ID {classroom_id} not found."
+        super().__init__(status_code=404, detail=detail)
+
+
+class LaboratoryNotFoundException(CompaxException):
+    def __init__(self, laboratory_id: str):
+        detail = f"Laboratory with ID {laboratory_id} not found."
+        super().__init__(status_code=404, detail=detail)
+
+
+class OfficeNotFoundException(CompaxException):
+    def __init__(self, office_id: str):
+        detail = f"Office with ID {office_id} not found."
+        super().__init__(status_code=404, detail=detail)
+
+
+class UserNotFoundException(CompaxException):
+    def __init__(self, user_id: UUID):
+        detail = f"User with ID {user_id} not found."
+        super().__init__(status_code=404, detail=detail)
+
+
+class InvalidCredentialsException(CompaxException):
+    def __init__(self):
+        super().__init__(status_code=401, detail="Invalid credentials.")
+
+
+class UnauthorizedException(CompaxException):
+    def __init__(self):
+        super().__init__(status_code=403, detail="Unauthorized access.")
+
+
+class BookingNotFoundException(CompaxException):
+    def __init__(self, booking_id: str):
+        detail = f"Booking with ID {booking_id} not found."
+        super().__init__(status_code=404, detail=detail)
