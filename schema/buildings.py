251423343cb54074f480e99833a21fb2b530c7d8
blackprince001 <appiahboaduprince@gmail.com>
2023-07-22 21:27:05 +0000

    fix fastapi type inference issues

--- schema/buildings.py
+++ schema/buildings.py
@@ -4,10 +4,10 @@ from pydantic import BaseModel
 class Building(BaseModel):
     id: int
     name: str
-    image_url: str
+    image_url: list[str]
     goem: str
     building_id: int
     building_type: str
-    n_floors: int | 0
-    n_classrooms: int | 0
-    n_labs: int | 0
+    n_floors: int
+    n_classrooms: int
+    n_labs: int
