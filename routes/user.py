60352ebacc9ce3be154ade75d51c23ea9ea744da
blackprince001 <appiahboaduprince@gmail.com>
2023-07-22 21:36:00 +0000

    remove isort pre-commit

--- routes/user.py
+++ routes/user.py
@@ -3,33 +3,67 @@ from uuid import UUID
 from fastapi import APIRouter, Depends
 from psycopg import Connection
 
+import compax_api.utils
 from compax_api.database import get_db_conn
-from compax_api.errors import UserNotFoundException
+from compax_api.errors import (
+    UserNameNotFoundException,
+    UserNotFoundException,
+    UserReferenceFoundException,
+)
+from schema.user import UserCreate
 
 user = APIRouter()
 
 
-@user.get("/i/", tags=["users"])
+@user.get("/i/{reference}", tags=["users"])
 async def get_user_by_reference(
     reference: int, connection: Connection = Depends(get_db_conn)
 ):
-    # insert connection query in here.
-    res = connection.execute()
+    # create script to extract uuid
+    res = compax_api.utils._get_parse_and_execute(
+        "get_user_username.sql", {"student_reference": reference}, connection
+    )
 
     if res is None:
-        raise UserNotFoundException(user_id=reference)
+        raise UserReferenceFoundException(ref=reference)
 
     # handle errors properly
     return res
 
 
-@user.get("/i/", tags=["users"])
+@user.get("/i/{uuid}", tags=["users"])
 async def get_user_by_uuid(uuid: UUID, connection: Connection = Depends(get_db_conn)):
-    # insert connection query in here.
-    res = connection.execute()
+    # create script to extract uuid
+    res = compax_api.utils._get_parse_and_execute(
+        "get_user_username.sql", {"id": uuid}, connection
+    )
 
     if res is None:
         raise UserNotFoundException(user_id=uuid)
 
     # handle errors properly
     return res
+
+
+@user.get("/i/{username}", tags=["users"])
+async def get_user_by_username(
+    username: str, connection: Connection = Depends(get_db_conn)
+):
+    res = compax_api.utils._get_parse_and_execute(
+        "get_user_username.sql", {"username": username}, connection
+    )
+
+    if res is None:
+        raise UserNameNotFoundException(username=username)
+
+    return res
+
+
+@user.post("/i/new", tags=["users"])
+async def create_user(
+    new_user: UserCreate, connection: Connection = Depends(get_db_conn)
+):
+    new_user.username += "@st.knust.edu.gh"
+    compax_api.utils._insert_parse_and_execute(
+        filename="insert_into_users.sql", payload=new_user.dict(), conn=connection
+    )
