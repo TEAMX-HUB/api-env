e9046d7530ab21cd04c990bd62ac7476398ded58
blackprince001 <appiahboaduprince@gmail.com>
2023-07-22 21:29:42 +0000

    composed new user crud methods

--- routes/ratings.py
+++ routes/ratings.py
@@ -1,3 +1,8 @@
 from fastapi import APIRouter
 
 ratings = APIRouter()
+
+# GET /ratings/classrooms/{id}: Retrieves ratings and reviews for a specific classroom.
+# GET /ratings/laboratories/{id}: Retrieves ratings and reviews for a specific laboratory
+# POST /ratings/classrooms/{id}: Adds a new rating and review for a specific classroom.
+# POST /ratings/laboratories/{id}: Adds a new rating and review for a specific laboratory.
