cc338a33f021773429926d45b0e025f9fb786ffb
blackprince001 <appiahboaduprince@gmail.com>
2023-07-23 08:50:30 +0000

    complete schema for scheduling tables

--- schema/schedule.py
+++ schema/schedule.py
@@ -1,5 +1,34 @@
 from pydantic import BaseModel
+from datetime import date, time
 
 
-class Schedule(BaseModel):
-    pass
+class AcademicEventWeek(BaseModel):
+    id: int
+    weekday: str
+    department: str
+    schedule_id: int
+    track_id: int
+
+
+class AcademicTrack(BaseModel):
+    id: int
+    semester: int
+    sem_starts: date
+    sem_ends: date
+    academic_year: str
+
+
+class SessionSchedule(BaseModel):
+    id: int
+    course_code: str
+    course_name: str
+    year_group: int
+    schedule_time: int
+    class_location: int
+    lab_location: int
+
+
+class SessionTime(BaseModel):
+    id: int
+    start_time: time
+    end_time: time
