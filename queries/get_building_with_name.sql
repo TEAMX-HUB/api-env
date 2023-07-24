9ce0736f1be1e6264dea69333546e33eb85900c5
blackprince001 <appiahboaduprince@gmail.com>
2023-07-24 15:34:13 +0000

    add building get queries

---
+++ queries/get_building_with_name.sql
@@ -0,0 +1,5 @@
+select *,
+       ST_X(ST_Transform(ST_SetSRID(geom, 32630), 4326)) as longitude,
+       ST_Y(ST_Transform(ST_SetSRID(geom, 32630), 4326)) as latitude
+from public.buildings
+where buildings.name like %(name)s;
