65764452520a212e70bede6d4fa012aa5041c141
blackprince001 <appiahboaduprince@gmail.com>
2023-07-24 19:27:43 +0000

    initialize ratings feature endpoints

--- routes/ratings.py
+++ routes/ratings.py
@@ -1,8 +1,66 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from psycopg import Connection
+from compax_api.database import get_db_conn
 
 ratings = APIRouter()
 
-# GET /ratings/classrooms/{id}: Retrieves ratings and reviews for a specific classroom.
-# GET /ratings/laboratories/{id}: Retrieves ratings and reviews for a specific laboratory
-# POST /ratings/classrooms/{id}: Adds a new rating and review for a specific classroom.
-# POST /ratings/laboratories/{id}: Adds a new rating and review for a specific laboratory.
+
+@ratings.get("/ratings/", tags=["ratings"])
+async def get_all_ratings(connection: Connection = Depends(get_db_conn)):
+    pass
+
+
+@ratings.get("/rating/classroom/{id}", tags=["ratings"])
+async def get_rating_for_classroom(
+    classroom_id: int, connection: Connection = Depends(get_db_conn)
+):
+    pass
+
+
+@ratings.get("/rating/lab/{id}", tags=["ratings"])
+async def get_rating_for_lab(
+    lab_id: int, connection: Connection = Depends(get_db_conn)
+):
+    pass
+
+
+@ratings.get("/rating/building/{id}", tags=["ratings"])
+async def get_rating_for_building(
+    classroom_id: int, connection: Connection = Depends(get_db_conn)
+):
+    pass
+
+
+@ratings.get("/rating/office/{id}", tags=["ratings"])
+async def get_rating_for_office(
+    classroom_id: int, connection: Connection = Depends(get_db_conn)
+):
+    pass
+
+
+@ratings.post("/rating/new/classroom/{id}", tags=["ratings"])
+async def create_rating_for_classroom(
+    classroom_id: int, connection: Connection = Depends(get_db_conn)
+):
+    pass
+
+
+@ratings.post("/rating/new/lab/{id}", tags=["ratings"])
+async def create_rating_for_lab(
+    lab_id: int, connection: Connection = Depends(get_db_conn)
+):
+    pass
+
+
+@ratings.post("/rating/new/building/{id}", tags=["ratings"])
+async def create_rating_for_building(
+    classroom_id: int, connection: Connection = Depends(get_db_conn)
+):
+    pass
+
+
+@ratings.post("/rating/new/office/{id}", tags=["ratings"])
+async def create_rating_for_office(
+    classroom_id: int, connection: Connection = Depends(get_db_conn)
+):
+    pass
