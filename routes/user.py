0133b51250248029ddd81ecf934a469e08887ca2
blackprince001 <appiahboaduprince@gmail.com>
2023-07-24 20:01:11 +0000

    add user validation test

--- routes/user.py
+++ routes/user.py
@@ -1,14 +1,16 @@
-from uuid import UUID
+from uuid import UUID, uuid4
 
 from fastapi import APIRouter, Depends
 from psycopg import Connection
 
 import compax_api.utils
+from compax_api.security import Password
 from compax_api.database import get_db_conn
 from compax_api.errors import (
     UserNameNotFoundException,
     UserNotFoundException,
     UserReferenceFoundException,
+    AuthInvalidCredentialsException,
 )
 from schema.user import UserCreate
 
@@ -19,7 +21,6 @@ user = APIRouter()
 def get_user_by_reference(
     reference: int, connection: Connection = Depends(get_db_conn)
 ):
-    # create script to extract uuid
     res = compax_api.utils._get_one_and_execute_params(
         "get_user_reference.sql", {"student_reference": reference}, connection
     )
@@ -32,7 +33,6 @@ def get_user_by_reference(
 
 @user.get("/i/{uuid}", tags=["users"])
 async def get_user_by_uuid(uuid: UUID, connection: Connection = Depends(get_db_conn)):
-    # create script to extract uuid
     res = compax_api.utils._get_one_and_execute_params(
         "get_user_uuid.sql", {"id": uuid}, connection
     )
@@ -58,8 +58,31 @@ async def get_user_by_username(
 
 
 @user.post("/i/new", tags=["users"])
-def create_user(new_user: UserCreate, connection: Connection = Depends(get_db_conn)):
+async def create_user(
+    new_user: UserCreate, connection: Connection = Depends(get_db_conn)
+):
+    new_user.id = uuid4()
     new_user.username += "@st.knust.edu.gh"
-    compax_api.utils._insert_parse_and_execute(
+    new_user.password = Password.hash(password=new_user.password)
+    results = compax_api.utils._insert_one_and_execute_params(
         filename="insert_into_users.sql", payload=new_user.dict(), conn=connection
     )
+    if results:
+        raise AuthInvalidCredentialsException
+    return results
+
+
+@user.get("/i/pd_check/{reference}", tags=["users"])
+async def check_user_validity(
+    reference: int, password: str, connection: Connection = Depends(get_db_conn)
+):
+    user = get_user_by_reference(reference=reference, connection=connection)
+    if user is None:
+        raise UserReferenceFoundException(ref=reference)
+
+    data = UserCreate(**user)
+
+    if Password.verify(plain=password, hashed=data.password):
+        return True
+    else:
+        raise UserNotFoundException(message="User with password cannot be found!")
