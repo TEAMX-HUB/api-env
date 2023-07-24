f4a3d1d855b2084ee411924121a4df8aae448ff8
blackprince001 <appiahboaduprince@gmail.com>
2023-07-24 15:41:48 +0000

    leave unresolved user authentication issues

--- routes/auth.py
+++ routes/auth.py
@@ -6,7 +6,11 @@ import routes.admin
 import routes.user
 from compax_api.config import get_settings
 from compax_api.database import get_db_conn
-from compax_api.errors import AuthInvalidCredentialsException
+from compax_api.errors import (
+    AuthInvalidCredentialsException,
+    UserReferenceFoundException,
+    UserNotFoundException,
+)
 from compax_api.security import Password
 from schema.user import AdminCreate, ExamOfficerCreate, UserCreate
 
@@ -20,22 +24,24 @@ supabase: Client = create_client(supabase_url=url, supabase_key=key)
 
 @auth.get("/auth/signin", tags=["auth"])
 async def sign_in(
-    username: str, password: str, connection: Connection = Depends(get_db_conn)
+    reference: str, password: str, connection: Connection = Depends(get_db_conn)
 ):
-    user = routes.user.get_user_by_username(username=username, connection=connection)
+    user = routes.user.get_user_by_reference(reference=reference, connection=connection)
+    if user is None:
+        raise UserReferenceFoundException(ref=reference)
 
-    # extract user hashed password from GET
-    # handle errors from GET if user not found
-    if Password.verify(plain=password, hashed=""):
-        pass
+    data = UserCreate(**user[0])
 
-    res = await supabase.auth.sign_in_with_password(
-        {
-            "email": username,
-            "password": Password.hash(password=password),
-            "email_confirm": False,
-        }
-    )
+    if Password.verify(plain=password, hashed=data.password):
+        res = await supabase.auth.sign_in_with_password(
+            {
+                "email": data.username,
+                "password": password,
+                "email_confirm": False,
+            }
+        )
+    else:
+        raise UserNotFoundException(message="User with password cannot be found!")
     return res
 
 
@@ -50,6 +56,7 @@ async def sign_up(new_user: UserCreate, connection: Connection = Depends(get_db_
     auth_response = _supabase_authenticate(new_user=new_user)
     extracted_id = auth_response.dict()["user"]["id"]
     new_user.id = extracted_id
+    new_user.password = Password.hash(new_user.password)
     routes.user.create_user(new_user, connection)
 
 
