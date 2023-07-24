e84cbd2dc895fd757c0fb153cead2b5914a5b522
blackprince001 <appiahboaduprince@gmail.com>
2023-07-24 15:41:48 +0000

    leave unresolved user authentication issues

--- routes/user.py
+++ routes/user.py
@@ -16,18 +16,17 @@ user = APIRouter()
 
 
 @user.get("/i/{reference}", tags=["users"])
-async def get_user_by_reference(
+def get_user_by_reference(
     reference: int, connection: Connection = Depends(get_db_conn)
 ):
     # create script to extract uuid
     res = compax_api.utils._get_parse_and_execute(
-        "get_user_username.sql", {"student_reference": reference}, connection
+        "get_user_reference.sql", {"student_reference": reference}, connection
     )
 
     if res is None:
         raise UserReferenceFoundException(ref=reference)
 
-    # handle errors properly
     return res
 
 
@@ -35,21 +34,21 @@ async def get_user_by_reference(
 async def get_user_by_uuid(uuid: UUID, connection: Connection = Depends(get_db_conn)):
     # create script to extract uuid
     res = compax_api.utils._get_parse_and_execute(
-        "get_user_username.sql", {"id": uuid}, connection
+        "get_user_uuid.sql", {"id": uuid}, connection
     )
 
     if res is None:
         raise UserNotFoundException(user_id=uuid)
 
     # handle errors properly
-    return res
+    return dict(res)
 
 
 @user.get("/i/{username}", tags=["users"])
 async def get_user_by_username(
     username: str, connection: Connection = Depends(get_db_conn)
 ):
-    res = compax_api.utils._get_parse_and_execute(
+    res = compax_api.utils._get_parse_and_execute_one(
         "get_user_username.sql", {"username": username}, connection
     )
 
@@ -60,9 +59,7 @@ async def get_user_by_username(
 
 
 @user.post("/i/new", tags=["users"])
-async def create_user(
-    new_user: UserCreate, connection: Connection = Depends(get_db_conn)
-):
+def create_user(new_user: UserCreate, connection: Connection = Depends(get_db_conn)):
     new_user.username += "@st.knust.edu.gh"
     compax_api.utils._insert_parse_and_execute(
         filename="insert_into_users.sql", payload=new_user.dict(), conn=connection
