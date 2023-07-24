0ede430bd3be83fe15659695fe7e4fe60cfba5e3
blackprince001 <appiahboaduprince@gmail.com>
2023-07-24 15:35:34 +0000

    add offices get queries

---
+++ queries/get_office_with_name.sql
@@ -0,0 +1,5 @@
+select *,
+       ST_X(ST_Transform(ST_SetSRID(geom, 32630), 4326)) as longitude,
+       ST_Y(ST_Transform(ST_SetSRID(geom, 32630), 4326)) as latitude
+from public.offices
+where offices.name like %(name)s;
