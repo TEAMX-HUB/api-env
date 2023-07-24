434e057398c9a4f4910b1120a559042803aa897a
blackprince001 <appiahboaduprince@gmail.com>
2023-07-24 19:26:29 +0000

    fix query bug

--- queries/get_office_with_name.sql
+++ queries/get_office_with_name.sql
@@ -2,4 +2,4 @@ select *,
        ST_X(ST_Transform(ST_SetSRID(geom, 32630), 4326)) as longitude,
        ST_Y(ST_Transform(ST_SetSRID(geom, 32630), 4326)) as latitude
 from public.offices
-where offices.name like %(name)s;
+where offices.staff_personnel like %(staff_personnel)s;
