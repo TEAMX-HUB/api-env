2d7a0112920a3c280423c5b38bdc2a6c85b772d3
blackprince001 <appiahboaduprince@gmail.com>
2023-07-25 14:20:52 +0000

    align ratings scheam to database

--- schema/ratings.py
+++ schema/ratings.py
@@ -1,14 +1,51 @@
-from uuid import uuid4 as UUID
-
 from pydantic import BaseModel
 
 
 class Rating(BaseModel):
     id: int
-    user_id: UUID
     building_id: int
     classroom_id: int
     office_id: int
+    lab_id: int
+    comments: str
+    rate_value: int
+
+
+class RateOffice(Rating):
+    id: int
+    building_id: int = None
+    classroom_id: int = None
+    office_id: int
+    lab_id: int = None
+    comments: str
+    rate_value: int
+
+
+class RateBuilding(Rating):
+    id: int
+    building_id: int
+    classroom_id: int = None
+    office_id: int = None
+    lab_id: int = None
+    comments: str
+    rate_value: int
+
+
+class RateClassroom(Rating):
+    id: int
+    building_id: int = None
+    classroom_id: int
+    office_id: int = None
+    lab_id: int = None
+    comments: str
+    rate_value: int
+
+
+class RateLaboratory(Rating):
+    id: int
+    building_id: int = None
+    classroom_id: int = None
+    office_id: int = None
     lab_id: int
     comments: str
     rate_value: int
