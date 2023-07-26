4ea7791238c3ab9f3d27333565b71b09220e8d26
blackprince001 <appiahboaduprince@gmail.com>
2023-07-26 16:44:25 +0000

    fix bug

--- queries/get_all_ratings.sql
+++ queries/get_all_ratings.sql
@@ -1,12 +1,8 @@
-SELECT  comments
-       ,rate_value
-       ,
+SELECT  
+    comments,
+    rate_value
 FROM public.ratings
-LEFT JOIN buildings b
-ON ratings.building_id = b.id
-LEFT JOIN classrooms c
-ON b.id = c.building_id
-LEFT JOIN offices o
-ON b.id = o.building_id
-LEFT JOIN laboratories l
-ON b.id = l.building_id;
+    LEFT JOIN buildings b ON ratings.building_id = b.id
+    LEFT JOIN classrooms c ON b.id = c.building_id
+    LEFT JOIN offices o ON b.id = o.building_id
+    LEFT JOIN laboratories l ON b.id = l.building_id;
