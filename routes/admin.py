23f8f0d93d84c5526d08fce88df87e653dade65e
blackprince001 <appiahboaduprince@gmail.com>
2023-07-27 07:01:07 +0000

    fix admin and exam_officer endpoints

--- routes/admin.py
+++ routes/admin.py
@@ -3,6 +3,7 @@ from psycopg import Connection
 
 import compax_api.utils
 from compax_api.database import get_db_conn
+from compax_api.errors import AuthInvalidCredentialsException
 from schema.user import AdminCreate, ExamOfficerCreate
 
 admin = APIRouter()
@@ -12,17 +13,21 @@ admin = APIRouter()
 async def create_admin(
     new_user: AdminCreate, connection: Connection = Depends(get_db_conn)
 ):
-    new_user.username += "@st.knust.edu.gh"
-    compax_api.utils._insert_parse_and_execute(
+    results = compax_api.utils._insert_one_and_execute_params(
         filename="insert_into_users.sql", payload=new_user.dict(), conn=connection
     )
+    if results:
+        raise AuthInvalidCredentialsException
+    return results
 
 
 @admin.post("/i/officer/new", tags=["users"])
 async def create_officer(
     new_user: ExamOfficerCreate, connection: Connection = Depends(get_db_conn)
 ):
-    new_user.username += "@st.knust.edu.gh"
-    compax_api.utils._insert_parse_and_execute(
+    results = compax_api.utils._insert_one_and_execute_params(
         filename="insert_into_users.sql", payload=new_user.dict(), conn=connection
     )
+    if results:
+        raise AuthInvalidCredentialsException
+    return results
